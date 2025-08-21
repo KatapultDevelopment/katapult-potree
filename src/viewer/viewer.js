41ada1e7d4760fb1befb641fb3ea4046c1265bf8
Brian Freed <bfreed@katapultengineering.com>
2025-08-21 18:50:12 -0400

    commented out console logs

--- src/viewer/viewer.js
+++ src/viewer/viewer.js
@@ -1406,7 +1406,7 @@ export class Viewer extends EventDispatcher {
 
 	initThree() {
 
-		console.log(`initializing three.js ${THREE.REVISION}`);
+		// console.log(`initializing three.js ${THREE.REVISION}`);
 
 		let width = this.renderArea.clientWidth;
 		let height = this.renderArea.clientHeight;
